46088146fe58ba106d683a788474b21c8effb3fd
arpli(李正) <arpli@tencent.com>
2020-07-22 21:22:34 +0800

    up

--- Code/02_Signleton.cpp
+++ Code/02_Signleton.cpp
@@ -1,73 +1,99 @@
 #include <iostream>
+#include <mutex>
+#include <thread>
 using namespace std;
-/* 
-剑指offer-2 题目：单例模式
 
-*/
-
-/*
-思路：
-
- */
-
-// 简单的 不完善的模式 初始版本 饿汉模式
-// 只创建了对象 没有释放 存在内存泄漏
-class Signleton1{
+// 恶汉模式 绝对安全 在创建对象的时候就进行初始化
+class Singleton1{
 public:
-    static Signleton1* getInstance(){
-        if(instance1 == nullptr){
-            instance1 = new Signleton1();
-        }
-        return instance1;
-    }
-private:
-    static Signleton1 *instance1;// 静态指针
-    
-    // 构造函数
-    Signleton1(){}
-    // 析构函数
-    ~Signleton1(){}
-    // 拷贝构造
-    Signleton1(const Signleton1 &){}
-    // 赋值运算符
-    Signleton1 &operator=(const Signleton1 &){}
-};
-// 静态指针 类外初始化
-Signleton1 *Signleton1::instance1 = nullptr;
-
-
-// 单线程 安全版本
-//程序结束的时候 自动调用Deletor的析构函数
-class Signleton2{
-public:
-    Signleton2 *getInstance()
+    static Singleton1* getInstance()
     {
-        if(instance2 != nullptr){
-            instance2 =new Signleton2();
-        }
-        return instance2;
+        return Singleton1;
     }
 private:
-    class Deletor{
-    public: 
-        ~Deletor(){
-            if(Signleton2::instance2 != nullptr){
-                delete Signleton2::instance2;
+    Singleton1(){}
+    Singleton1(const Singleton1&);
+    Singleton1& operator=(const Singleton1&);
+    static Singleton1 *instance;
+};
+Singleton1 Singleton1::instance = new Singleton1;
+
+
+
+
+// 懒汉模式 多线程下 非线程安全的
+class Singleton
+{
+public:
+    static Singleton* getInstance(){
+        if (instance == nullptr) {
+            instance = new *Singleton;
+            return instance;
+        }
+    }
+private:
+    static Singleton *instance;
+    Singleton(){}
+    Singleton(const Singleton &);
+    Singleton& operator=(cosnt Singleton&);
+
+};
+Singleton* Singleton::instance = nullptr;
+
+
+mutex mt;
+// 利用双检锁实现的线程安全的懒汉模式
+class Singleton2
+{
+public:
+    static Singleton2* getInstance()
+    {
+        if (instance == nullptr) {
+            lock_guard<mutex> lg(mt);
+            if (instance == nullptr) {
+                instance = new Singleton2;
+                return instance;
+            }
+        }
+    }
+private:
+    Singleton2(){}
+    Singleton2(const Singleton2&);
+    Singleton2& operator=(const Singleton2&);
+    static Singleton2 *instance;
+};
+Singleton2* Singleton2::instance = nullptr;
+
+// 懒汉模式  双检锁 + 自动回收
+class Singleton3()
+{
+public:
+    static Singleton3* getInstance()
+    {
+        if (instance == nullptr) {
+            lock_guard<mutex> lg(mt);
+            if (instance == nullptr) {
+                instance = new instance;
+                return instance;
+            }
+        }
+    }
+    class GC{
+        public:
+        ~GC(){
+            if (Singleton3::instance) {
+                delete Singleton3::instance;
             }
         }
     };
-    static Deletor deletor;
 private:
-    static Signleton2 *instance2;
-
-    Signleton2(){}
-    ~Signleton2(){}
-    Signleton2(const Signleton2 &){}
-    Signleton2 & operator=(const Signleton2 &){}
-
-};
-Signleton2 *Signleton2::instance2 = nullptr;
-
+    Singleton3(){}
+    Singleton3(const Singleton3&);
+    Singleton3& operator(const Singleton3&);
+    static Singleton3 *instance;
+    static GC gc;
+}
+Singleton3* Singleton3::instance = nullptr;
 
 // 懒汉模式和饿汉模式
 // 饿汉式单例模式：在类加载时就完成了初始化，所以类加载比较慢、获取对象的速度快、以空间换取时间模式、线程安全、
